96502d7895a101279dafddf8833b6cad27a09851
Smol_220 <asmolich@yandex.ru>
2022-04-04 13:22:20 +0300

    second commit

--- js/login.js
+++ js/login.js
@@ -2,3 +2,8 @@ function getData() {
   let name = document.getElementById('name').value;
   console.log('name-field:' + name, 'password-field:');
 }
+
+function getData() {
+  let name = document.getElementById('name2').value;
+  console.log('password-field:');
+}
