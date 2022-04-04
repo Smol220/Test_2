25173630d23f917a06ef37c92c793497b981ed32
KONSTANTSIN OKULICH <konstantsin.okulich@consultant.turkcell.com.tr>
2022-04-04 14:18:17 +0300

    password error message added

--- js/login.js
+++ js/login.js
@@ -1,5 +1,15 @@
+// hide for first time
 function getData() {
   let name = document.getElementById('name').value;
   let password = document.getElementById('password').value;
-  console.log('name-field:' + name, 'password-field:' + password);
+  let passwordErr = document.getElementsByClassName('password-err')[0];
+  // console.log('name-field:' + name, 'password-field:' + password);
+
+  console.log('колличество символов в поле c паролем' + password.length);
+  
+  // прячем или показываем поле с ошибкой под паролем
+  // <= меньше или равно
+  if (password.length <= 5) passwordErr.hidden = false;
+  else passwordErr.hidden = true;
+   
 }
